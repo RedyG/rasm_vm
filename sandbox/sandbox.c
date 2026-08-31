728b5894aa43a4f2994d9130485773428188adf2
RedyG <103346770+RedyG@users.noreply.github.com>
2026-08-30 20:43:36 -0400

    transition to dual project ( exe and lib )

--- sandbox/sandbox.c
+++ sandbox/sandbox.c
@@ -4,20 +4,16 @@
 #include <stdio.h>
 #include "Disassembler.h"
 
-
 int main() {
 	clock_t start_time = clock();
 
 	Module module = get_module("C:/Users/gagno/OneDrive/Documents/Programmation/c#/RedyLangCompiler/Sandbox/output/program.rasm");
 
-	//disassemble(module);
 	disassemble(module);
 	thread_start(module, 2);
 
 	double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
 	printf("Done in %f seconds\n", elapsed_time);
-
-
 
 	return 0;
 }
